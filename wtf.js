97b8ee7e0b721f71c60de5ec319f58668de2657b
Arjun Sharma <rgzsharma@gmail.com>
2012-10-27 11:17:52 -0600

    Removed Underscore.js from the project by extracting only the componenets necessary

--- wtf.js
+++ wtf.js
@@ -5,7 +5,7 @@
 //
 // With thanks to the awesome people of:
 // Backbone.js - http://www.backbonejs.org
-// Underscore.js - http://www.underscore.js
+// Underscore.js - http://www.underscorejs.org
 // Jasmine - http://pivotal.github.com/jasmine
 // Pivotal Labs - http://www.pivotallabs.com
 
@@ -18,8 +18,40 @@ var WTFConfigs = {
 	var root = this;
 	var WTF;
 	WTF = root.WTF = function() { this.initialize.apply(this, arguments); }
-	var _ = root._;
-	_.extend(WTF.prototype, { initialize: function(){} });
+//	var _ = root._;
+
+	// START UnderscoreJS fragments *************
+	// http://underscorejs.org
+	var breaker = {};
+	var u_each = function(obj, iterator, context) {
+		if (obj == null) return;
+		if (Array.prototype.forEach && obj.forEach === Array.prototype.forEach) {
+			obj.forEach(iterator, context);
+		} else if (obj.length === +obj.length) {
+			for (var i = 0, l = obj.length; i < l; i++) {
+				if (iterator.call(context, obj[i], i, obj) === breaker) return;
+			}
+		} else {
+			for (var key in obj) {
+				if (Object.prototype.hasOwnProperty.call(obj, key)) {
+					if (iterator.call(context, obj[key], key, obj) === breaker) return;
+				}
+			}
+		}
+	};
+
+	var u_extend = function(obj) {
+		u_each(Array.prototype.slice.call(arguments, 1), function(source) {
+			for (var prop in source) {
+				obj[prop] = source[prop];
+			}
+		});
+		return obj;
+	};
+	// END Underscore JS fragments *************
+
+
+	u_extend(WTF.prototype, { initialize: function(){} });
 
 	var extend = function (protoProps, classProps) {
 		var child = inherits(this, protoProps, classProps);
@@ -31,11 +63,11 @@ var WTFConfigs = {
 
 	var inherits = function(parent, protoProps, staticProps) {
 		var child = function(){ parent.apply(this, arguments); };
-		_.extend(child, parent);
+		u_extend(child, parent);
 		ctor.prototype = parent.prototype;
 		child.prototype = new ctor();
-		if (protoProps) _.extend(child.prototype, protoProps);
-		if (staticProps) _.extend(child, staticProps);
+		if (protoProps) u_extend(child.prototype, protoProps);
+		if (staticProps) u_extend(child, staticProps);
 		child.prototype[WTFConfigs.superName] = parent.prototype;
 		return child;
 	};

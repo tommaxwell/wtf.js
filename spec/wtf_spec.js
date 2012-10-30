44e73e96bb5b2bebb057807b08e7b3c0f2cead58
Arjun Sharma <rgzsharma@gmail.com>
2012-10-29 18:53:55 -0600

    Wrote tests for getting WTFness without inheriting from WTF

--- spec/wtf_spec.js
+++ spec/wtf_spec.js
@@ -9,12 +9,124 @@ describe("WTF", function () {
 	  expect(new WTF).toBeInstanceOf(WTF)
 	});
 
-	describe("#extend", function() {
-		var STFU, initializeSpy, staticMethodSpy, instanceMethodSpy;
+	describe("sharing extend without inheritance", function() {
+		var BaseClass, SubClass, MyObject, subClassInitSpy,
+				initSpy, instanceMethodSpy, accessSuperSpy, staticFuncSpy,
+				subClassFunctionSpy;
+
+		beforeEach(function() {
+			subClassInitSpy = jasmine.createSpy();
+			initSpy = jasmine.createSpy();
+			accessSuperSpy = jasmine.createSpy();
+			instanceMethodSpy = jasmine.createSpy();
+			staticFuncSpy = jasmine.createSpy();
+			subClassFunctionSpy = jasmine.createSpy();
+
+			BaseClass = function() {
+				this.init.apply(this, arguments);
+			};
+
+			BaseClass.extend = WTF.extend;
+
+			SubClass = BaseClass.extend({
+				i_am_a: "SubClass",
+				init: function(arg) {
+				  subClassInitSpy(arg);
+				},
+				subClassFunction: function(arg) {
+				  subClassFunctionSpy(this.i_am_a);
+				}
+			}, {
+				staticProperty: "bar",
+				staticMethod: function() {
+
+				}
+			});
+
+			MyObject = SubClass.extend({
+				i_am_a: "MyObject",
+				init: function(arg) {
+					initSpy(arg);
+				},
+				instanceMethod: function(arg) {
+				  instanceMethodSpy(arg);
+				},
+				accessSuperMethod: function(arg) {
+					this.super.subClassFunction.call(this);
+				}
+			}, {
+				staticProperty: "fu",
+				staticMethod: function() {
+					staticFuncSpy();
+				}
+			});
+		});
+
+		it("should extend properly after sharing #extend", function() {
+			var sub = new SubClass("fubar");
+			expect(subClassInitSpy).toHaveBeenCalledWith("fubar");
+			expect(sub.i_am_a).toBe("SubClass");
+
+			var obj = new MyObject("hello");
+			expect(initSpy).toHaveBeenCalledWith("hello");
+			expect(obj.i_am_a).toBe("MyObject");
+		});
+
+		it("should set instance methods correctly", function() {
+			var sub = new SubClass("hey");
+			expect(SubClass.subClassFunction).toBeUndefined();
+			expect(sub.subClassFunction).toBeDefined();
+			sub.subClassFunction();
+			expect(subClassFunctionSpy).toHaveBeenCalledWith("SubClass");
+
+			var obj = new MyObject();
+			expect(MyObject.instanceMethod).toBeUndefined();
+			expect(obj.instanceMethod).toBeDefined();
+			obj.instanceMethod("bar");
+			expect(instanceMethodSpy).toHaveBeenCalledWith("bar");
+		});
+
+		it("should have static properties and extend them properly", function() {
+			expect(SubClass.staticProperty).toBe("bar");
+			expect(MyObject.staticProperty).toBe("fu");
+		});
+
+		it("should have static methods and extend them properly", function() {
+			expect(staticFuncSpy).not.toHaveBeenCalled();
+			MyObject.staticMethod();
+			expect(staticFuncSpy).toHaveBeenCalled();
+		});
+
+		it("should be an instance of itself and its parents, but not WTF", function() {
+			expect(new SubClass).toBeInstanceOf(SubClass);
+			expect(new SubClass).toBeInstanceOf(BaseClass);
+			expect(new SubClass).not.toBeInstanceOf(WTF);
+
+			expect(new MyObject).toBeInstanceOf(MyObject);
+			expect(new MyObject).toBeInstanceOf(SubClass);
+			expect(new MyObject).toBeInstanceOf(BaseClass);
+			expect(new MyObject).not.toBeInstanceOf(WTF);
+		});
+
+		it("should be able to use 'super", function() {
+			var obj = new MyObject();
+			obj.instanceMethod(obj.super.i_am_a);
+			expect(instanceMethodSpy).toHaveBeenCalledWith("SubClass");
+		});
+	});
+
+	describe("inheriting from WTF", function() {
+		var STFU, initializeSpy, staticMethodSpy, instanceMethodSpy,
+				ROFL, childInitializeSpy, childInstanceMethodSpy, childStaticMethodSpy;
+
 	  beforeEach(function() {
 			initializeSpy = jasmine.createSpy();
 			instanceMethodSpy = jasmine.createSpy();
 			staticMethodSpy = jasmine.createSpy();
+			childInitializeSpy = jasmine.createSpy();
+			childInstanceMethodSpy = jasmine.createSpy();
+			childStaticMethodSpy = jasmine.createSpy();
+
 			STFU = WTF.extend({
 				type: "STFU",
 				initialize: function() {
@@ -29,23 +141,49 @@ describe("WTF", function () {
 				  staticMethodSpy();
 				}
 			});
+
+			ROFL = STFU.extend({
+				type: "ROFL",
+				initialize: function() {
+					childInitializeSpy();
+				},
+				instanceMethod: function() {
+					childInstanceMethodSpy();
+				}
+			}, {
+				staticProperty: "lol",
+				staticMethod: function() {
+					childStaticMethodSpy();
+				}
+			});
 	  });
 
 		it("should be an instance of itself and of WTF", function() {
 		  expect(new STFU()).toBeInstanceOf(WTF);
 			expect(new STFU()).toBeInstanceOf(STFU);
+
+			expect(new ROFL()).toBeInstanceOf(ROFL);
+			expect(new ROFL()).toBeInstanceOf(STFU);
+			expect(new ROFL()).toBeInstanceOf(WTF);
 		});
 
 		it("should still call the initialize function when instantiated", function() {
 			expect(initializeSpy).not.toHaveBeenCalled();
 			new STFU();
 			expect(initializeSpy).toHaveBeenCalled();
+
+			expect(childInitializeSpy).not.toHaveBeenCalled();
+			new ROFL();
+			expect(childInitializeSpy).toHaveBeenCalled();
 		});
 
 		it("should set instance properties correctly", function() {
 			var stfuInstance = new STFU();
 		  expect(stfuInstance.type).toBe("STFU");
 			expect(STFU.type).toBeUndefined();
+
+			var roflInstance = new ROFL();
+			expect(roflInstance.type).toBe("ROFL");
 		});
 
 		it("should set instance methods correctly", function() {
@@ -54,125 +192,81 @@ describe("WTF", function () {
 			expect(stfuInstance.instanceMethod).toBeDefined();
 			stfuInstance.instanceMethod();
 			expect(instanceMethodSpy).toHaveBeenCalled();
+
+			var roflInstance = new ROFL();
+			expect(childInstanceMethodSpy).not.toHaveBeenCalled();
+			roflInstance.instanceMethod();
+			expect(childInstanceMethodSpy).toHaveBeenCalled();
 		});
 
 		it("should set static properties correctly", function() {
 			expect(STFU.staticProperty).toBe("fubar");
+
+			expect(ROFL.staticProperty).toBe("lol");
 		});
 
 		it("should set static methods correctly", function() {
 		  expect(staticMethodSpy).not.toHaveBeenCalled();
 			STFU.staticMethod();
 			expect(staticMethodSpy).toHaveBeenCalled();
+
+			expect(childStaticMethodSpy).not.toHaveBeenCalled();
+			ROFL.staticMethod();
+			expect(childStaticMethodSpy).toHaveBeenCalled();
 		});
+	});
 
-		describe("extending multiple levels", function() {
-			var ROFL, childInitializeSpy, childInstanceMethodSpy, childStaticMethodSpy;
-			beforeEach(function() {
-				childInitializeSpy = jasmine.createSpy();
-				childInstanceMethodSpy = jasmine.createSpy();
-				childStaticMethodSpy = jasmine.createSpy();
-				ROFL = STFU.extend({
-					type: "ROFL",
-					initialize: function() {
-						childInitializeSpy();
-					},
-					instanceMethod: function() {
-					  childInstanceMethodSpy();
-					}
-				}, {
-					staticProperty: "lol",
-					staticMethod: function() {
-						childStaticMethodSpy();
-					}
-				});
+	describe("extra super tests", function() {
+		var Animal, Human, Clown, animalFunctionSpy, humanFunctionSpy, clownFunctionSpy;
+
+		beforeEach(function() {
+			animalFunctionSpy = jasmine.createSpy();
+			humanFunctionSpy = jasmine.createSpy();
+			clownFunctionSpy = jasmine.createSpy();
+
+			Animal = WTF.extend({
+				type: "animal",
+				doSomething: function() {
+					animalFunctionSpy(this.type);
+				}
 			});
 
-			it("should be an instance of all of its ancestors", function() {
-			  expect(new ROFL()).toBeInstanceOf(ROFL);
-				expect(new ROFL()).toBeInstanceOf(STFU);
-				expect(new ROFL()).toBeInstanceOf(WTF);
+			Human = Animal.extend({
+				type: "human",
+				doSomething: function() {
+					this.super.doSomething.call(this);
+					humanFunctionSpy(this.super.type);
+				}
 			});
 
-			it("should still call the initialize function when instantiated", function() {
-				expect(childInitializeSpy).not.toHaveBeenCalled();
-				new ROFL();
-				expect(childInitializeSpy).toHaveBeenCalled();
-			});
-
-			it("should override instance properties of its parent", function() {
-				var roflInstance = new ROFL();
-			  expect(roflInstance.type).toBe("ROFL");
-			});
-
-			it("should override instance methods of its parent", function() {
-				var roflInstance = new ROFL();
-			  expect(childInstanceMethodSpy).not.toHaveBeenCalled();
-				roflInstance.instanceMethod();
-				expect(instanceMethodSpy).not.toHaveBeenCalled();
-				expect(childInstanceMethodSpy).toHaveBeenCalled();
-			});
-
-			it("should override static properties of its parent", function() {
-				expect(ROFL.staticProperty).toBe("lol");
-			});
-
-			it("should override static methods of its parent", function() {
-				expect(childStaticMethodSpy).not.toHaveBeenCalled();
-				ROFL.staticMethod();
-				expect(childStaticMethodSpy).toHaveBeenCalled();
+			Clown = Human.extend({
+				type: "clown",
+				doSomething: function() {
+					this.super.super.doSomething.call(this);
+					clownFunctionSpy(this.super.super.type);
+				}
 			});
 		});
 
-		describe("the super method", function() {
-		  var Animal, Human, Clown, animalFunctionSpy, humanFunctionSpy, clownFunctionSpy;
-			beforeEach(function() {
-				animalFunctionSpy = jasmine.createSpy();
-				humanFunctionSpy = jasmine.createSpy();
-				clownFunctionSpy = jasmine.createSpy();
-			  Animal = WTF.extend({
-					type: "animal",
-					doSomething: function() {
-					  animalFunctionSpy(this.type);
-					}
-				});
+		it("should be able to access the parent by calling this.super", function() {
+			var dog = new Animal();
+			dog.doSomething();
+			expect(animalFunctionSpy).toHaveBeenCalledWith("animal");
+			expect(humanFunctionSpy).not.toHaveBeenCalled();
+			animalFunctionSpy.reset();
 
-				Human = Animal.extend({
-					type: "human",
-					doSomething: function() {
-						this.super.doSomething.call(this);
-					  humanFunctionSpy(this.super.type);
-					}
-				});
+			var jennifer = new Human();
+			jennifer.doSomething();
+			expect(animalFunctionSpy).toHaveBeenCalledWith("human");
+			expect(humanFunctionSpy).toHaveBeenCalledWith("animal");
+		});
 
-				Clown = Human.extend({
-					type: "clown",
-					doSomething: function() {
-					  this.super.super.doSomething.call(this);
-						clownFunctionSpy(this.super.super.type);
-					}
-				});
-			});
-
-			it("should be able to access the parent by calling this.super", function() {
-				var dog = new Animal();
-				dog.doSomething();
-				expect(animalFunctionSpy).toHaveBeenCalledWith("animal");
-				expect(humanFunctionSpy).not.toHaveBeenCalled();
-				animalFunctionSpy.reset();
-			  var jennifer = new Human();
-				jennifer.doSomething();
-				expect(animalFunctionSpy).toHaveBeenCalledWith("human");
-				expect(humanFunctionSpy).toHaveBeenCalledWith("animal");
-			});
-
-			it("should be able to chain super calls", function() {
-			  var bozo = new Clown();
-				bozo.doSomething();
-				expect(animalFunctionSpy).toHaveBeenCalledWith("clown");
-				expect(humanFunctionSpy).not.toHaveBeenCalled();
-				expect(clownFunctionSpy).toHaveBeenCalledWith("animal");
-			});
+		it("should be able to chain super calls", function() {
+			var bozo = new Clown();
+			bozo.doSomething();
+			expect(animalFunctionSpy).toHaveBeenCalledWith("clown");
+			expect(humanFunctionSpy).not.toHaveBeenCalled();
+			expect(clownFunctionSpy).toHaveBeenCalledWith("animal");
 		});
 	});
 });
